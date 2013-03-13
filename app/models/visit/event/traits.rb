3758a9f24f41f6a8476c56a33c39c15fd9493721
Leni Mayo <leni@learnable.com>
2013-03-14 09:18:53 +1100

    Visit::Event::Traits

---
+++ app/models/visit/event/traits.rb
@@ -0,0 +1,38 @@
+module Visit
+  class Event::Traits
+
+    def initialize(ve)
+      @ve = ve
+      @path = Visit::Event.path_from_url(ve.url)
+    end
+
+    def to_h
+      { }.tap do |h|
+        h.merge! get_label_sublabel
+        h.merge! get_utm
+        h.merge! get_gclid
+      end
+    end
+    
+    private
+
+    def get_utm
+      str = [ :utm_term, :utm_source, :utm_medium, :utm_content, :utm_campaign ].map do |k|
+        h = { http_method: :get, re: Regexp.new("[&|?]#{k.to_s}=(.*?)(&.*|)$"), label: :utm, has_sublabel: true }
+        m = Visit::Event::Matcher.from_hash h
+        m.matches?(@ve.http_method, @path) ? m.sublabel : ""
+      end.join("_")
+      str =~ /^_*$/ ? {} : { utm: str }
+    end
+
+    def get_gclid
+      h = { http_method: :get, re: /[&|?]gclid=(.*?)(&.*|)$/, label: :gclid, has_sublabel: true }
+      m = Visit::Event::Matcher.from_hash h
+      m.matches?(@ve.http_method, @path) ?  { gclid: m.sublabel } : {}
+    end
+
+    def get_label_sublabel
+       (m = Visit::Event::Matcher.first_match(@ve.http_method, @path)) ? { label: m.label, sublabel: m.sublabel } : {}
+    end
+  end
+end
