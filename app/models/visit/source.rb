7f1055cfd9ef35e668d0dad014cd317f2017c6ea
Leni Mayo <leni@learnable.com>
2013-03-14 19:34:21 +1100

    Visit::Event has_many Visit::Source

---
+++ app/models/visit/source.rb
@@ -0,0 +1,15 @@
+module Visit
+  class Source < ActiveRecord::Base
+    self.table_name_prefix = 'visit_'
+
+    belongs_to :visit_event
+    belongs_to :key,   :class_name => "VisitSourceValue", foreign_key: :k_id
+    belongs_to :value, :class_name => "VisitSourceValue", foreign_key: :v_id
+
+    validates :k_id, :v_id, :visit_event_id, :presence => true
+    validates :k_id, :uniqueness => {
+                    :scope => :visit_event_id,
+                    :message => "should happen a max of once per visit_event_id" }
+
+  end
+end
