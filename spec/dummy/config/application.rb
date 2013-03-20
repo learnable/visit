6d2cb3f1698b78165cfaa164924c61fc7c7b323e
Matthew Hayter <matthew.hayter@sitepointgroup.com>
2013-03-20 17:44:47 +1100

    Bundler requires Rails.env group by default.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -8,7 +8,7 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-Bundler.require
+Bundler.require(:default, Rails.env)
 require "visit"
 
 module Dummy
