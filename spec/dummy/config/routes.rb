8680ed945a121dd0b2ad3a09490a9b5651059dee
Matthew Hayter <matthew.hayter@sitepointgroup.com>
2013-03-20 17:01:03 +1100

    Articles scaffold in dummy app.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :articles
+
+
   namespace :visit do; get "/pageviews", to: "pageviews#new", as: :pageviews end
 
   namespace :visit do; get "/tag.gif",   to: "tag#create",    as: :tag       end
