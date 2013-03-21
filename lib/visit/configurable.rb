a1de62d3911c213a2e0afe13b81a9902b6cdde35
Matthew Hayter <matthew.hayter@sitepointgroup.com>
2013-03-21 15:07:24 +1100

    Rewrite of Configurable. Delgate to parent app to handle exceptions.

--- lib/visit/configurable.rb
+++ lib/visit/configurable.rb
@@ -1,25 +1,37 @@
 module Visit
+  # This class is designed to be opened and overriden by the parent app.
   class Configurable
     class << self
 
+      # Parent app should override this to return an array of label-matchers,
+      # where each label-matcher has format
+      # [ :http_method, /path_reg_exp/, :label_name, has_sublabel_bool]
+      # For example, for an 'alpha_signups' label:
+      # [ :post, /^\/alpha_signups/, :alpha_signups, false ]
       def labels
-        throw_exception
+        raise_exception_delegator
       end
 
+      # Parent app should override this to return an array of ruby reg-exps,
+      # specifing a black-list of paths to be ignored, for example,
+      # to ignore requests to the /api/* path:
+      # /^\/api/
       def ignorable
-        throw_exception
+        raise_exception_delegator
       end
 
       private
 
-      def throw_exception
+      def raise_exception_delegator
         begin
-          raise RuntimeError, "Visit::Configurable - expected this to be overridden by config/initializers'"
+          raise RuntimeError, "Visit::Configurable - expected a configurable method to be overridden by config/initializers"
         rescue => e
-          # TODO: put this back in
-          #CrashLog.notify e
-          puts e.to_s
+          catch_execption_delegate(e)
         end
+      end
+
+      def catch_exception_delegate(e)
+        raise e
       end
 
     end
