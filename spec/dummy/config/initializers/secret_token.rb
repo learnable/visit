acc674cd8295db1ac564760155ccd63c08b6185e
Matthew Hayter <matthew.hayter@sitepointgroup.com>
2013-03-26 11:08:53 +1100

    Rm boilerplate comments and files.

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = 'a1f5fa9f49638aa040b46e45c81340eeacb25d696b992dd3ce605d82b5402a846d743cc59f113efa2c735b723b673d8962933599fc143013949288e828d0a264'
