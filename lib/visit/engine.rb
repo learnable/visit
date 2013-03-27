238643218fe77419af1304c8a7a4ed841f83b9ff
Matthew Hayter <matthew.hayter@sitepointgroup.com>
2013-03-27 14:31:09 +1100

    Turn off Schema Plus's auto database-fiddling.

--- lib/visit/engine.rb
+++ lib/visit/engine.rb
@@ -4,6 +4,10 @@ module Visit
 
     initializer "visit.blah" do |app|
       ActionController::Base.send(:include, Visit::ControllerFilters)
+      SchemaPlus.setup do |config|
+        config.foreign_keys.auto_create = false;
+        config.foreign_keys.auto_index = false;
+      end
     end
 
   end
