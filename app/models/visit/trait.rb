73e4db938ebe711223d2edf4cf8aebe628a72bfb
Leni Mayo <leni@learnable.com>
2013-04-02 16:00:30 +1100

    Visit::Trait associations need :class_name => blah because of Visit:: module scope

--- app/models/visit/trait.rb
+++ app/models/visit/trait.rb
@@ -2,9 +2,9 @@ module Visit
   class Trait < ActiveRecord::Base
     self.table_name_prefix = 'visit_'
 
-    belongs_to :visit_event
-    belongs_to :key,   :class_name => "VisitTraitValue", foreign_key: :k_id
-    belongs_to :value, :class_name => "VisitTraitValue", foreign_key: :v_id
+    belongs_to :visit_event, :class_name => "Visit::Event"
+    belongs_to :key,   :class_name => "Visit::TraitValue", foreign_key: :k_id
+    belongs_to :value, :class_name => "Visit::TraitValue", foreign_key: :v_id
 
     validates :k_id, :v_id, :visit_event_id, :presence => true
     validates :k_id, :uniqueness => {
