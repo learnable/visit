40aefb55de1bfb0a724e091fda6a00c8a674a160
Leni Mayo <leni@learnable.com>
2013-04-10 09:59:22 +1000

    traits_spec.rb tests for ?glcid

--- spec/models/visit/event/traits_spec.rb
+++ spec/models/visit/event/traits_spec.rb
@@ -2,17 +2,20 @@ require 'spec_helper'
 require 'shared_gem_config'
 
 describe Visit::Event::Traits do
-  let(:ve) { create(:visit_event, url: "http://thishost.org/articles") }
+  let(:ve) { create(:visit_event, url: "http://thishost.org/articles?gclid=4455") }
   let(:traits) { Visit::Event::Traits.new(ve) }
 
   ##### Finally, the tests:
 
   describe "#to_h" do
-    it "should return a hash" do
+    it "returns a hash" do
       traits.to_h.kind_of?(Hash).should be_true
     end
-    it "returns the visit_event's label" do
+    it "has_key? :label" do
       traits.to_h[:label].should == :articles_index
+    end
+    it "has_key? :glcid " do
+      traits.to_h[:gclid].should == "4455"
     end
   end
 end
