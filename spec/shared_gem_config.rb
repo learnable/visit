4fac0fd7464276c42a35ba7580ed03458c7ab4c3
Leni Mayo <leni@learnable.com>
2013-04-10 10:20:14 +1000

    shared_gem_config.rb whitespace

--- spec/shared_gem_config.rb
+++ spec/shared_gem_config.rb
@@ -1,25 +1,27 @@
-
-# Configure the gem
 class Visit::Configurable
+
   def self.labels
-    [{
-      :http_method  => :get,
-      :re           => /^\/articles(\?.*|)$/,
-      :label        => :articles_index,
-      :has_sublabel => false
-    },
+    [
       {
-      :http_method  => :get,
-      :re           => /^\/articles\/(\d)/,
-      :label        => :article,
-      :has_sublabel => true
-    }
+        :http_method  => :get,
+        :re           => /^\/articles(\?.*|)$/,
+        :label        => :articles_index,
+        :has_sublabel => false
+      },
+      {
+        :http_method  => :get,
+        :re           => /^\/articles\/(\d)/,
+        :label        => :article,
+        :has_sublabel => true
+      }
     ]
   end
+
   def self.ignorable
     [
       /\/courses\/blah.js/,
       /\/system\/blah/,
     ]
   end
+
 end
