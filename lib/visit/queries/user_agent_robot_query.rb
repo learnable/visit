216fdd536f1c371640949c0525392b8dcf18d1ff
Leni Mayo <leni@learnable.com>
2013-04-25 06:00:25 +1000

    cosmetic formatting of SQL query

--- lib/visit/queries/user_agent_robot_query.rb
+++ lib/visit/queries/user_agent_robot_query.rb
@@ -7,8 +7,8 @@ module Visit
     def scoped
       stmt = %Q{
         INNER JOIN visit_traits user_agent_robot_vt
-          ON visit_events.id = user_agent_robot_vt.visit_event_id AND user_agent_robot_vt.k_id =
-            (select id from visit_trait_values where v = 'robot')
+          ON visit_events.id = user_agent_robot_vt.visit_event_id
+          AND user_agent_robot_vt.k_id = (select id from visit_trait_values where v = 'robot')
         INNER JOIN visit_trait_values user_agent_robot_vtv
           ON user_agent_robot_vtv.id = user_agent_robot_vt.v_id
       }
