d7bacc4122a4f1fff4c8a83086ab21122b4204fa
Leni Mayo <leni@learnable.com>
2013-04-25 23:39:52 +1000

    LabelledEventQuery LEFT OUTER JOINS capture2

--- lib/visit/queries/labelled_event_query.rb
+++ lib/visit/queries/labelled_event_query.rb
@@ -17,9 +17,15 @@ module Visit
             (select id from visit_trait_values where v = 'capture1')
         LEFT OUTER JOIN visit_trait_values capture1_vtv
           ON capture1_vtv.id = capture1_vt.v_id
+
+        LEFT OUTER JOIN visit_traits capture2_vt
+          ON visit_events.id = capture2_vt.visit_event_id AND capture2_vt.k_id =
+            (select id from visit_trait_values where v = 'capture2')
+        LEFT OUTER JOIN visit_trait_values capture2_vtv
+          ON capture2_vtv.id = capture2_vt.v_id
       }
       @relation.
-        select("visit_events.*, label_vtv.v as label, capture1_vtv.v as capture1").
+        select("visit_events.*, label_vtv.v as label, capture1_vtv.v as capture1, capture2_vtv.v as capture2").
         joins(stmt)
     end
 
