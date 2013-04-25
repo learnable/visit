d83daa95dfcc10cb27edfc26fcf56625f9351db4
Leni Mayo <leni@learnable.com>
2013-04-25 23:25:03 +1000

    improve matcher_spec.rb

--- spec/models/visit/event/matcher_spec.rb
+++ spec/models/visit/event/matcher_spec.rb
@@ -7,11 +7,11 @@ describe Visit::Event::Matcher do
     let(:path) { "/articles" }
 
     describe "matches?" do
-      it "matches http_method and path" do
+      it "http_method and path" do
         matcher.matches?("get", path).should be_true
       end
 
-      it 'does not match garbage' do
+      it "returns false when there's no match" do
         matcher.matches?("blah", "/art").should be_false
         matcher.matches?("post", "aldkcjka").should be_false
         matcher.matches?("get", "/article").should be_false
@@ -31,45 +31,45 @@ describe Visit::Event::Matcher do
     end
   end
 
-  context "matchdata_to_label_h" do
-    context "after matches?" do
-      context "to a regexp with no captures" do
-        let(:matcher) { Visit::Event::Matcher.new :get, %r{^/articles(\?.*|)$}, :articles_index }
+  context "after matches?" do
+    before { matcher.matches?("get", path).should be_true }
+
+    context "matchdata_to_label_h" do
+      context "when regexp has no captures" do
+        let(:matcher) { Visit::Event::Matcher.new :get, %r{^/articles(?:\?.*|)$}, :articles_index }
         let(:path) { "/articles" }
 
-        before { matcher.matches?("get", path).should be_true }
-
-        it "returns a hash with :label" do
+        it "returns a hash with key :label" do
           h = matcher.matchdata_to_label_h
 
           h.has_key?(:label).should be_true
           h[:label].should == :articles_index
+
+          h.size.should == 1
         end
       end
 
-      context "to a regexp with one capture" do
+      context "when regexp has one capture" do
         let(:matcher) { Visit::Event::Matcher.new :any, /^\/articles\/(\d+)/, :article }
         let(:path) { "/articles/123" }
 
-        before { matcher.matches?("get", path).should be_true }
-
-        it "returns a hash with :label and :capture1" do
+        it "returns a hash with keys :label and :capture1" do
           h = matcher.matchdata_to_label_h
 
           h.has_key?(:label).should be_true
           h.has_key?(:capture1).should be_true
           h[:label].should == :article
           h[:capture1].should == "123"
+
+          h.size.should == 2
         end
       end
       
-      context "to a regexp with two captures" do
+      context "when regexp has two captures" do
         let(:matcher) { Visit::Event::Matcher.new :any, %r{^/articles/(\d+)/(\d+)}, :article }
         let(:path) { "/articles/123/456" }
 
-        before { matcher.matches?("get", path).should be_true }
-
-        it "returns a hash with :label, :capture1 and :capture2" do
+        it "returns a hash with keys :label, :capture1 and :capture2" do
           h = matcher.matchdata_to_label_h
 
           h.has_key?(:label).should be_true
@@ -78,17 +78,15 @@ describe Visit::Event::Matcher do
           h[:label].should == :article
           h[:capture1].should == "123"
           h[:capture2].should == "456"
+
+          h.size.should == 3
         end
       end
     end
-  end
 
-  context "matchdata_to_value_h" do
-    context "after matches?" do
+    context "matchdata_to_value_h" do
       let(:matcher) { Visit::Event::Matcher.new :any, /^\/articles\/(\d+)/, :article }
       let(:path) { "/articles/123" }
-
-      before { matcher.matches?("get", path) }
 
       it "returns a hash whose key is label and value is capture1" do
         h = matcher.matchdata_to_value_h
