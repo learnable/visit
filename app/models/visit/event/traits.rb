1259b884fd8663999e90c0240cacd65dd88aaa02
Leni Mayo <leni@learnable.com>
2013-04-25 10:05:55 +1000

    Visit::Event::Traits refactored
    get_label_sublabel, get_utm and get_gclid replaced with
    get_match_first and get_match_all

--- app/models/visit/event/traits.rb
+++ app/models/visit/event/traits.rb
@@ -8,30 +8,24 @@ module Visit
 
     def to_h
       { }.tap do |h|
-        h.merge! get_label_sublabel
-        h.merge! get_utm
-        h.merge! get_gclid
+        h.merge! get_match_first
+        h.merge! get_match_all.inject(&:merge)
         h.merge! get_user_agent_robot
       end
     end
 
     private
 
-    def get_utm
-      str = [ :utm_term, :utm_source, :utm_medium, :utm_content, :utm_campaign ].map do |k|
-        m = Visit::Event::Matcher.new :get, Regexp.new("[&|?]#{k.to_s}=(.*?)(&.*|)$"), :utm, true
-        m.matches?(@ve.http_method, @path) ? m.sublabel : ""
-      end.join("_")
-      str =~ /^_*$/ ? {} : { utm: str }
+    def get_match_first
+       c = Event::MatcherCollection.new Visit::Configurable.labels_match_first
+
+       c.match_first_to_h(@ve.http_method, @path)
     end
 
-    def get_gclid
-      m = Visit::Event::Matcher.new :get, /[&|?]gclid=(.*?)(&.*|)$/, :gclid, true
-      m.matches?(@ve.http_method, @path) ?  { gclid: m.sublabel } : {}
-    end
+    def get_match_all
+       c = Event::MatcherCollection.new Visit::Configurable.labels_match_all
 
-    def get_label_sublabel
-       (m = Visit::Event::Matcher.first_match(@ve.http_method, @path)) ? { label: m.label, sublabel: m.sublabel } : {}
+       c.match_all_to_a(@ve.http_method, @path)
     end
 
     def get_user_agent_robot
