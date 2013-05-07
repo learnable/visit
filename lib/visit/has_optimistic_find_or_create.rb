2de8a60317b89fac8ee83320f202027b6a406b3d
Leni Mayo <leni@learnable.com>
2013-05-08 09:14:18 +1000

    module HasOptimisticFindOrCreate

---
+++ lib/visit/has_optimistic_find_or_create.rb
@@ -0,0 +1,23 @@
+module HasOptimisticFindOrCreate
+  def optimistic_find_or_create_by_v_id(v)
+    Visit::Configurable.cache.fetch_prefix_value(cache_key_prefix, v) do
+      optimistic_find_or_create_by_v(v).id
+    end
+  end
+
+  private
+
+  def optimistic_find_or_create_by_v(v)
+    begin
+      self.find_or_create_by_v(v)
+    rescue ActiveRecord::StatementInvalid => e
+      # multiple workers using find_or_create_by can result in a race condition
+      # in which case, assume the row exists and return it
+      self.find_by_v(v)
+    end
+  end
+
+  def cache_key_prefix
+    @cache_key_prefix ||= "#{self.to_s}.find_by_v.id"
+  end
+end
