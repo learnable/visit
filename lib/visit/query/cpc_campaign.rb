e72af1b02967c3a15f5eafe92be6f544ca756528
Leni Mayo <leni@learnable.com>
2013-05-10 22:21:17 +1000

    Visit::Query::CpcCampaign

---
+++ lib/visit/query/cpc_campaign.rb
@@ -0,0 +1,17 @@
+module Visit
+  class Query
+    class CpcCampaign < Query
+      def scoped
+        super.
+          where("utm_medium_vtv.v = 'cpc'").
+      end
+
+      protected
+
+      def stmt
+        stmt_join_trait("INNER JOIN",      'utm_medium') +
+        stmt_join_trait("LEFT OUTER JOIN", 'utm_campaign')
+      end
+    end
+  end
+end
