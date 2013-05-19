fb85354d8ed1f65141983467722d4f005e8e06b2
Leni Mayo <leni@learnable.com>
2013-05-19 15:02:20 +1000

    Visit::Cache::Key respects Configurable.case_insensitive_string_comparison

--- lib/visit/cache/key.rb
+++ lib/visit/cache/key.rb
@@ -1,10 +1,21 @@
 module Visit
   class Cache
     class Key < Struct.new(:part_a, :part_b)
+      def initialize(a,b)
+        self[:part_a] = case_convert a.to_s
+        self[:part_b] = case_convert b.to_s
+      end
+
       def key
         "#{part_a}:#{part_b}"
       end
       alias_method :to_s, :key
+
+      private
+
+      def case_convert(str)
+        Configurable.case_insensitive_string_comparison ? str.downcase : str
+      end
     end
   end
 end
