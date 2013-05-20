6ca9909817f737bbd89cad8ef8dd6887cd62ce57
Leni Mayo <leni@learnable.com>
2013-05-20 11:52:24 +1000

    Visit::Cache::Dalli.has_key?

--- lib/visit/cache/dalli.rb
+++ lib/visit/cache/dalli.rb
@@ -5,9 +5,17 @@ module Visit
         super cache
       end
 
+      def has_key?(key)
+        raise "expected Cache::key" unless key.instance_of? Visit::Cache::Key
+
+        @cache.get(key.to_s) == false ? false : true
+      end
+
       def fetch(key, options = {})
         if cache
-          cache.fetch(key, options) do
+          raise "expected Cache::key" unless key.instance_of? Visit::Cache::Key
+
+          cache.fetch(key.to_s, options) do
             yield
           end
         else
