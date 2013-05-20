9a28400bbdbb370e4843f1968cf3e6ac08518c8e
Leni Mayo <leni@learnable.com>
2013-05-21 01:00:31 +1000

    Cache::Null.has_key?

--- lib/visit/cache/null.rb
+++ lib/visit/cache/null.rb
@@ -5,6 +5,12 @@ module Visit
         super
       end
 
+      def has_key?(key)
+        raise "expected Cache::Key" unless key.instance_of? Visit::Cache::Key
+
+        false
+      end
+
       def fetch(key, options = {})
         yield
       end
