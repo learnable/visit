9c022f17422ac000ea7b477d9570fa5a4f90fae4
Leni Mayo <leni@learnable.com>
2013-05-20 11:48:17 +1000

    Visit::Cache::Memory.has_key? spec

--- spec/lib/cache/memory_spec.rb
+++ spec/lib/cache/memory_spec.rb
@@ -2,29 +2,49 @@ require 'spec_helper'
 
 describe Visit::Cache::Memory do
   let(:cache) { Visit::Cache::Memory.new }
+  let(:key) { Visit::Cache::Key.new("", 222) }
 
   context "#fetch" do
     it "should evaluate to it's block" do
-      tmp = cache.fetch(222) { 111 }
+      tmp = cache.fetch(key) { 111 }
       tmp.should == 111
     end
 
     context "should hit cache" do
       it "on the first fetch of a key but not the second" do
         hit = ""
-        cache.fetch(222) { 111; hit += "a" }
-        cache.fetch(222) { 111; hit += "b" }
+        cache.fetch(key) { 111; hit += "a" }
+        cache.fetch(key) { 111; hit += "b" }
         hit.should == "a"
       end
 
       it "on the first fetch of a key after #clear" do
         hit = ""
-        cache.fetch(222) { 111; hit += "a" }
-        cache.fetch(222) { 111; hit += "b" }
+        cache.fetch(key) { 111; hit += "a" }
+        cache.fetch(key) { 111; hit += "b" }
         cache.clear
-        cache.fetch(222) { 111; hit += "c" }
+        cache.fetch(key) { 111; hit += "c" }
         hit.should == "ac"
       end
+    end
+  end
+  
+  context "#has_key?" do
+    it "should return false when key not present" do
+      cache.has_key?(key).should be_false
+    end
+    it "should return true when key present" do
+      cache.fetch(key) { 111 }
+      cache.has_key?(key).should be_true
+    end
+    it "should have no side effects" do
+      cache.fetch(key) { 111 }
+
+      hit = false
+      result = cache.fetch(key) { 111; hit = true }
+
+      result.should == 111
+      hit.should be_false
     end
   end
 end
