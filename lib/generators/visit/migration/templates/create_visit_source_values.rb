ebe0627b69da3119ffd386c6dda9162cef87a1ff
Leni Mayo <leni@learnable.com>
2013-05-21 00:56:15 +1000

    CreateVisitSourceValues migration index on :v isn't unique

--- lib/generators/visit/migration/templates/create_visit_source_values.rb
+++ lib/generators/visit/migration/templates/create_visit_source_values.rb
@@ -6,6 +6,6 @@ class CreateVisitSourceValues < ActiveRecord::Migration
       t.timestamp :created_at
     end
 
-    add_index :visit_source_values, :v, :unique => true
+    add_index :visit_source_values, :v
   end
 end
