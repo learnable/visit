f2f95e3e98a2eb36ba4d7c596e1f52822598b7ed
Leni Mayo <leni@learnable.com>
2013-05-21 00:59:37 +1000

    Cache::Key.key - optimise for speed

--- lib/visit/cache/key.rb
+++ lib/visit/cache/key.rb
@@ -4,10 +4,13 @@ module Visit
       def initialize(a,b)
         self[:part_a] = case_convert a.to_s
         self[:part_b] = case_convert b.to_s
+
+        self[:part_a].freeze
+        self[:part_b].freeze
       end
 
       def key
-        "#{part_a}:#{part_b}"
+        @key ||= "#{part_a}:#{part_b}" # this gets called a lot - optimise for speed
       end
       alias_method :to_s, :key
 
