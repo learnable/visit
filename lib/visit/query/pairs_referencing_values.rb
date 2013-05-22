fc4dae6a0d0c66a2b8ed585bd6dc9b1b16537927
Leni Mayo <leni@learnable.com>
2013-05-23 02:58:33 +1000

    Visit::Query::PairsReferencingValues

---
+++ lib/visit/query/pairs_referencing_values.rb
@@ -0,0 +1,15 @@
+module Visit
+  class Query
+    class PairsReferencingValues < Query
+      def initialize(model_class, ids)
+        @model_class = model_class
+        @ids = ids
+      end
+
+      def scoped
+        @model_class.
+          where("k_id IN (?) OR v_id IN (?)", @ids, @ids)
+      end
+    end
+  end
+end
