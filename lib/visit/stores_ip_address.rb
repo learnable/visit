4fe588c75b0010d2f163919922e5860c05227a24
Leni Mayo <leni@learnable.com>
2013-05-23 02:49:23 +1000

    StoresIpAddress writer accepts a Fixnum as an rvalue
    I got tired of not being able to write:
    visit_event1.remote_ip = visit_event_2.remote_ip

--- lib/visit/stores_ip_address.rb
+++ lib/visit/stores_ip_address.rb
@@ -14,7 +14,7 @@ module Visit
 
         define_method(:"#{attr}=") do |ip|
           require "ipaddr"
-          write_attribute :remote_ip, IPAddr.new(ip).to_i
+          write_attribute :remote_ip, (ip.instance_of?(Fixnum) ? ip : IPAddr.new(ip).to_i)
         end
 
       end
