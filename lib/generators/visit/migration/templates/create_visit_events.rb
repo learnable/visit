bc4277d52a040d34eb24b33b4779ad647b9898f4
Leni Mayo <leni@learnable.com>
2013-05-26 00:05:11 +1000

    CreateVisitEvents migration template isn't irreversible

--- lib/generators/visit/migration/templates/create_visit_events.rb
+++ lib/generators/visit/migration/templates/create_visit_events.rb
@@ -1,5 +1,5 @@
 class CreateVisitEvents < ActiveRecord::Migration
-  def change
+  def up
     create_table :visit_events do |t|
       t.integer  "http_method_enum"
       t.integer  "url_id", :references => :visit_source_values
@@ -11,9 +11,19 @@ class CreateVisitEvents < ActiveRecord::Migration
       t.timestamp :created_at
     end
 
+    # t.binary ought to do the job here, except that
+    # in Rails 3, t.binary maps to BLOB.
+    #
+    # In Rails 4, t.binary maps to VARCHAR so when all the gem's apps are on Rails 4
+    # this migration can be made more rails-y.
+    #
     execute "ALTER TABLE visit_events ADD token VARBINARY(16) DEFAULT NULL AFTER url_id"
 
     add_index :visit_events, :token
     add_index :visit_events, :created_at
+  end
+
+  def down
+    drop_table :visit_events
   end
 end
