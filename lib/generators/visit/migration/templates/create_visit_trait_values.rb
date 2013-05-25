bd6da46460dfc5785f202ba22d14e59c1ca20624
Leni Mayo <leni@learnable.com>
2013-05-25 15:58:53 +1000

    visit_trait_values.v :limit => 2048

--- lib/generators/visit/migration/templates/create_visit_trait_values.rb
+++ lib/generators/visit/migration/templates/create_visit_trait_values.rb
@@ -1,7 +1,7 @@
 class CreateVisitTraitValues < ActiveRecord::Migration
   def change
     create_table :visit_trait_values do |t|
-      t.string :v, :null => false
+      t.string :v, :null => false, :limit => 2048
 
       t.timestamp :created_at
     end
