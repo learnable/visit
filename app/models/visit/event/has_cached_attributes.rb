e4fc05fefa7259a4fdc95469f4f25993c167e40b
Leni Mayo <leni@learnable.com>
2013-05-29 05:28:12 +1000

    Event's url, user_agent and referer methods move to module Event::HasCachedAttributes

---
+++ app/models/visit/event/has_cached_attributes.rb
@@ -0,0 +1,35 @@
+module Visit
+  module Event::HasCachedAttributes
+    def url
+      fetch_from_cache(url_id) do
+        nil_or_value visit_source_values_url
+      end
+    end
+
+    def user_agent
+      fetch_from_cache(user_agent_id) do
+        nil_or_value visit_source_values_user_agent
+      end
+    end
+
+    def referer
+      fetch_from_cache(referer_id) do
+        nil_or_value visit_source_values_referer
+      end
+    end
+
+    private
+
+    def fetch_from_cache(id)
+      key = Cache::Key.new("SourceValue.find", id)
+
+      Configurable.cache.fetch(key) do
+        yield
+      end
+    end
+
+    def nil_or_value(vsv)
+      vsv.nil? ? nil : vsv.v
+    end
+  end
+end
