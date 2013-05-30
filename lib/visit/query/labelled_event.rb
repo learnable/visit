e8b4a3c7a8324e6376a5f8a51b2384f836b947a2
Leni Mayo <leni@learnable.com>
2013-05-30 16:40:47 +1000

    Query::LabelledEvent contructor takes an array of symbols
    The symbols are expected to correspond to traits.
    Two things happen:
    1. add LEFT OUTER JOINs
    2. add SELECTs

--- lib/visit/query/labelled_event.rb
+++ lib/visit/query/labelled_event.rb
@@ -3,24 +3,36 @@ module Visit
 
     def initialize(a_traits = [], relation = Event.scoped)
       @relation = relation
-      @a_traits = a_traits
+      @a_traits = a_traits.map(&:to_s)
     end
 
     def scoped
-      @relation.
-        joins(stmt)
+      relation = @relation.joins(stmt)
+      relation = add_joins_for_traits(relation)
+      relation = add_selects_for_traits(relation)
+      relation
     end
 
     protected
 
     def stmt
-      stmt_join_trait("INNER JOIN", 'label') + "\n" + stmts_for_traits
+      stmt_join_trait("INNER JOIN", 'label')
     end
 
-    def stmts_for_traits
-      @a_traits.map do |k|
-        stmt_join_trait("LEFT OUTER JOIN", k.to_s)
-      end.join("")
+    def add_joins_for_traits(relation)
+      @a_traits.empty? ?
+        relation :
+        relation.joins(@a_traits.map {|k| stmt_join_trait("LEFT OUTER JOIN", k) }.join(""))
+    end
+
+    def add_selects_for_traits(relation)
+      @a_traits.empty? ?
+        relation :
+        relation.select("visit_events.*, label_vtv.v as label, #{select_string_for_traits}")
+    end
+
+    def select_string_for_traits
+      @a_traits.map {|k| "#{k}_vtv.v as #{k}" }.join(",")
     end
 
   end
