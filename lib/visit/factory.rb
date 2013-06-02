eab151e7a9b980bc64aaf239470b179645fd75ae
Leni Mayo <leni@learnable.com>
2013-06-02 22:37:51 +1000

    Factory holds an instance of class Cache
    And passes it to (some of) the Collect constructors.

--- lib/visit/factory.rb
+++ lib/visit/factory.rb
@@ -1,52 +1,56 @@
 module Visit
   class Factory
-    class << self
-      def delete_traits
-        Visit::Trait.delete_all
-        Visit::TraitValue.delete_all
-      end
+    def initialize
+      @cache = Configurable.cache.instance_of?(Visit::Cache::Null) ?
+        Visit::Cache::Memory.new :
+        Configurable.cache
+    end
 
-      def recreate_traits
-        Configurable.cache.clear
+    def self.delete_traits
+      Visit::Trait.delete_all
+      Visit::TraitValue.delete_all
+    end
 
-        delete_traits
+    def recreate_traits
+      Configurable.cache.clear
 
-        Visit::Event.
-          includes([:visit_source_values_url, :visit_source_values_user_agent, :visit_source_values_referer]).
-          find_in_batches do |a_event|
-            Visit::Factory.create_traits a_event.map { |event| { event: event } }
-          end
-      end
+      Factory.delete_traits
 
-      def run(a_request_payload_hash)
-        a_request_payload_hash.each { |rph| rph.symbolize_keys! }
+      Visit::Event.
+        includes([:visit_source_values_url, :visit_source_values_user_agent, :visit_source_values_referer]).
+        find_in_batches do |a_event|
+          Visit::Factory.create_traits a_event.map { |event| { event: event } }
+        end
+    end
 
-        collect = Collect::SourceValues.new Visit::SourceValue, a_request_payload_hash
-        collect.transform!
-        collect.import!
+    def run(a_request_payload_hash)
+      a_request_payload_hash.each { |rph| rph.symbolize_keys! }
 
-        collect = Collect::Events.new Visit::Event, a_request_payload_hash
-        collect.import!
-        a_request_payload_hash = collect.collection # contains :event
+      collect = Collect::SourceValues.new Visit::SourceValue, a_request_payload_hash, @cache
+      collect.transform!
+      collect.import!
 
-        collect = Collect::Sources.new Visit::Source, a_request_payload_hash
-        collect.transform!
-        collect.import!
+      collect = Collect::Events.new Visit::Event, a_request_payload_hash
+      collect.import!
+      a_request_payload_hash = collect.collection # contains :event
 
-        create_traits(a_request_payload_hash)
-      end
+      collect = Collect::Sources.new Visit::Source, a_request_payload_hash
+      collect.transform!
+      collect.import!
 
-      def create_traits(a_event)
-        collect_traits = Collect::Traits.new Visit::Trait, a_event
-        collect_traits.transform!
-        a_event = collect_traits.collection # contains :traits
+      create_traits(a_request_payload_hash)
+    end
 
-        collect_trait_values = Collect::TraitValues.new Visit::TraitValue, a_event
-        collect_trait_values.transform!
-        collect_trait_values.import!
+    def create_traits(a_event)
+      collect_traits = Collect::Traits.new Visit::Trait, a_event, @cache
+      collect_traits.transform!
+      a_event = collect_traits.collection # contains :traits
 
-        collect_traits.import!
-      end
+      collect_trait_values = Collect::TraitValues.new Visit::TraitValue, a_event, @cache
+      collect_trait_values.transform!
+      collect_trait_values.import!
+
+      collect_traits.import!
     end
   end
 end
