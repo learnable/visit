a4b4c18957490c4af3eab8621fa397b10305d377
Leni Mayo <leni@learnable.com>
2013-06-12 06:01:52 +1000

    delete useless comments

--- app/models/visit/source.rb
+++ app/models/visit/source.rb
@@ -7,11 +7,5 @@ module Visit
     belongs_to :value, :class_name => "Visit::SourceValue", foreign_key: :v_id
 
     validates :k_id, :v_id, :visit_event_id, :presence => true
-
-    # TODO - replace this with a sweeper
-    # validates :k_id, :uniqueness => {
-    #                :scope => :visit_event_id,
-    #                :message => "should happen a max of once per visit_event_id" }
-
   end
 end
