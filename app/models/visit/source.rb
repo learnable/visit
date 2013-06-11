865b4652ce4bbef045fb6fb02ccb1d28f793bf26
Leni Mayo <leni@learnable.com>
2013-06-12 06:01:00 +1000

    Visit::Source belongs_to :class_name fixups

--- app/models/visit/source.rb
+++ app/models/visit/source.rb
@@ -2,9 +2,9 @@ module Visit
   class Source < ActiveRecord::Base
     self.table_name_prefix = 'visit_'
 
-    belongs_to :visit_event
-    belongs_to :key,   :class_name => "VisitSourceValue", foreign_key: :k_id
-    belongs_to :value, :class_name => "VisitSourceValue", foreign_key: :v_id
+    belongs_to :visit_event, :class_name => "Visit::Event"
+    belongs_to :key,   :class_name => "Visit::SourceValue", foreign_key: :k_id
+    belongs_to :value, :class_name => "Visit::SourceValue", foreign_key: :v_id
 
     validates :k_id, :v_id, :visit_event_id, :presence => true
 
