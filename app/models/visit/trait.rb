e3a9fac557ecbd18602b9e01ec95edcd769e1b0d
Leni Mayo <leni@learnable.com>
2013-06-12 14:58:55 +1000

    whitespace

--- app/models/visit/trait.rb
+++ app/models/visit/trait.rb
@@ -3,8 +3,8 @@ module Visit
     self.table_name_prefix = 'visit_'
 
     belongs_to :visit_event, :class_name => "Visit::Event"
-    belongs_to :key,   :class_name => "Visit::TraitValue", foreign_key: :k_id
-    belongs_to :value, :class_name => "Visit::TraitValue", foreign_key: :v_id
+    belongs_to :key,         :class_name => "Visit::TraitValue", foreign_key: :k_id
+    belongs_to :value,       :class_name => "Visit::TraitValue", foreign_key: :v_id
 
     validates :k_id, :v_id, :visit_event_id, :presence => true
   end
