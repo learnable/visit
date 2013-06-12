3aad74dcc996ad9e92ed605fca1d151a3b4541da
Leni Mayo <leni@learnable.com>
2013-06-12 13:18:32 +1000

    move the Manage::destroy methods to DestroyUnused

---
+++ lib/visit/destroy_unused.rb
@@ -0,0 +1,37 @@
+module Visit
+  class DestroyUnused
+    class << self
+
+      def rows!
+        events!
+        sources!
+        source_values!
+      end
+
+      def events!
+        Event.includes(:visit_source_values_url).find_in_batches do |events|
+          ids = events.select { |event| event.ignorable? }.map(&:id)
+
+          Event.destroy ids
+
+          yield ids if block_given?
+        end
+      end
+
+      def sources!
+        Source.includes([:key, :value]).find_in_batches do |sources|
+          ids = sources.select { |source| source.in_use? }.map(&:id)
+
+          Source.destroy ids
+
+          yield ids if block_given?
+        end
+      end
+
+      def source_values!
+        # TODO
+      end
+
+    end
+  end
+end
