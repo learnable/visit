01607f9f38a9983056c01b2a5bbe7401082bea7f
Leni Mayo <leni@learnable.com>
2013-06-14 14:03:02 +1000

    HasTemporaryCache: s/original_cache/temporary_cache_instead_of/

--- lib/visit/has_temporary_cache.rb
+++ lib/visit/has_temporary_cache.rb
@@ -2,17 +2,17 @@ module Visit
   module HasTemporaryCache
     def temporary_cache_setup
       if Configurable.cache.instance_of? Visit::Cache::Null
-        @original_cache = Configurable.cache
+        @temporary_cache_instead_of = Configurable.cache
         Configurable.cache = Visit::Cache::Memory.new
       else
-        @original_cache = nil
+        @temporary_cache_instead_of = nil
       end
     end
 
     def temporary_cache_teardown
-      if !@original_cache.nil?
-        Configurable.cache = @original_cache
-        @original_cache = nil
+      if @temporary_cache_instead_of
+        Configurable.cache = @temporary_cache_instead_of
+        @temporary_cache_instead_of = nil
       end
     end
   end
