26bd10eed6afaad5098e6ae31819fc35de25af53
Leni Mayo <leni@learnable.com>
2013-06-19 08:02:55 +1000

    more Factory specs

--- spec/lib/factory_spec.rb
+++ spec/lib/factory_spec.rb
@@ -5,14 +5,40 @@ describe Visit::Factory do
     delete_all_visits
   end
 
-  context "events that have labels" do
-    it "create traits" do
-      h1 = new_request_payload_hash url: "http://e.org/articles"
-      h2 = new_request_payload_hash url: "http://e.org/articles/1"
+  context "with some requests" do
+    let(:h1) { new_request_payload_hash url: "http://e.org/articles" }
+    let(:h2) { new_request_payload_hash url: "http://e.org/articles/1" }
+    let(:h3) { new_request_payload_hash url: "http://e.org/articles" }
 
-      expect {
+    context "in one run of the factory" do
+      before { Visit::Factory.new.run [ h1, h2 ] }
+
+      it "Traits are created" do
+        Visit::Trait.count.should == 3
+      end
+
+      it "SourceValues are created" do
+        Visit::SourceValue.count.should == 6
+      end
+
+      it "TraitValues are created" do
+        Visit::TraitValue.count.should == 5
+      end
+    end
+
+    context "in two runs of the factory" do
+      before do
         Visit::Factory.new.run [ h1, h2 ]
-      }.to change { Visit::Trait.count }.by(3)
+        Visit::Factory.new.run [ h3 ]
+      end
+
+      it "not too many SourceValues are created" do
+        Visit::SourceValue.count.should == 6
+      end
+
+      it "not too many TraitValues are created" do
+        Visit::TraitValue.count.should == 5
+      end
     end
   end
 
