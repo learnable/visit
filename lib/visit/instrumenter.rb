3976de3617a251b1d1ccea9babf95b8cb05b13d1
Leni Mayo <leni@learnable.com>
2013-06-22 11:42:44 +1000

    Instrumenter::History is in instrumenter/history.rb

--- lib/visit/instrumenter.rb
+++ lib/visit/instrumenter.rb
@@ -49,52 +49,5 @@ module Visit
     def to_json
       JSON.generate @marks
     end
-
-    class History
-      def initialize(o)
-        @marks = marks_from o
-      end
-
-      def hostname
-        @marks.first["hostname"]
-      end
-
-      def pid
-        @marks.first["pid"]
-      end
-
-      def marks
-        @marks.slice(1, @marks.length)
-      end
-
-      def timeline
-        @marks.map { |h| Time.parse h["created_at"] }
-      end
-
-      def to_pretty_str
-        lines = []
-
-        marks.map do |mark|
-          lines << mark[:created_at]
-          mark.keys.select { |k| k != :created_at }.each do |k|
-            lines << "  #{k}: #{mark[k]}"
-          end
-        end
-
-        lines.join("\n")
-      end
-
-      private
-
-      def marks_from(o)
-        # o is either a string containing json or a instance of Visit::Log
-
-        JSON.parse(o.instance_of?(Visit::Log) ? o.message : o).map do |mark|
-          m = mark.symbolize_keys
-          m[:created_at] = Time.parse(m[:created_at]) if m.has_key?(:created_at)
-          m
-        end
-      end
-    end
   end
 end
