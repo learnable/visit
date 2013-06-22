7d97d1b9e41c3c2523190d736c41bcfeb1ee1e86
Leni Mayo <leni@learnable.com>
2013-06-22 11:49:38 +1000

    Log#to_instrumenter_history

--- app/models/visit/log.rb
+++ app/models/visit/log.rb
@@ -2,5 +2,9 @@ module Visit
   class Log < BaseModel
     attr_accessible :category
     attr_accessible :message
+
+    def to_instrumenter_history
+      Instrumenter::History.new self
+    end
   end
 end
