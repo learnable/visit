2e3b135f37902ebde733b35f2bd909354c75cbde
Leni Mayo <leni@learnable.com>
2013-06-23 16:22:38 +1000

    Onboarder adds RailsRequestContext to SerializedQueue :filling

--- lib/visit/onboarder.rb
+++ lib/visit/onboarder.rb
@@ -4,24 +4,42 @@ module Visit
       Configurable.ignorable.any? { |re| path =~ re }
     end
 
-    def self.accept_unless_ignorable(request)
-      # request is either RailsRequestContext or RequestPayload
-      #
-      unless request.ignorable?
+    def accept_unless_ignorable(rails_request_context)
+      raise "unexpected argument" unless rails_request_context.instance_of?(RailsRequestContext)
+
+      unless rails_request_context.ignorable?
         begin
-          queue = Configurable.serialized_queue.call "request_payload_hashes"
+          rails_request_context.must_insert = true
 
-          queue_length = queue.pipelined_rpush_and_return_length request.to_h
+          queue_filling.pipelined_rpush_and_return_length rails_request_context.to_h
 
-          if queue_length >= Configurable.bulk_insert_batch_size
-            values = queue.pipelined_lpop_and_clear(Configurable.bulk_insert_batch_size)
+          make_available(queue_filling) if queue_filling.full?
 
-            Configurable.create.call values
-          end
+          # TODO: remove references to queue_legacy once flippa has migrated
+          make_available(queue_legacy) if (queue_legacy.length > 0)
         rescue => e
           Configurable.notify.call e
         end
       end
+    end
+
+    private
+
+    def make_available(queue)
+      new_key = queue.renamenx_to_random_key
+
+      if !new_key.nil?
+        Configurable.serialized_queue.call(:available).rpush new_key
+        Configurable.create.call
+      end
+    end
+
+    def queue_filling
+      @queue_filling ||= Configurable.serialized_queue.call :filling
+    end
+
+    def queue_legacy
+      @queue_legacy ||= Configurable.serialized_queue.call "request_payload_hashes"
     end
   end
 end
