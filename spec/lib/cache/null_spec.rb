1581ecdc3c0ef592d154e046a0bb82585bfe7a40
Leni Mayo <leni@learnable.com>
2013-06-29 13:52:01 +1000

    fix null_spec.rb

--- spec/lib/cache/null_spec.rb
+++ spec/lib/cache/null_spec.rb
@@ -2,17 +2,18 @@ require 'spec_helper'
 
 describe Visit::Cache::Null do
   let(:cache) { Visit::Cache::Null.new }
+  let(:key) { Visit::Cache::Key.new("", 111) }
 
   context "#fetch" do
     it "should evaluate to it's block" do
-      tmp = cache.fetch(222) { 111 }
+      tmp = cache.fetch(key) { 111 }
       tmp.should == 111
     end
 
     it "should never hit cache" do
       hit = 0
-      cache.fetch(222) { 111; hit += 1 }
-      cache.fetch(222) { 111; hit += 1 }
+      cache.fetch(key) { 111; hit += 1 }
+      cache.fetch(key) { 111; hit += 1 }
       hit.should == 2
     end
   end
