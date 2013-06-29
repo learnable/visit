bba22d7396be3cff818266a187f08e475bd3121a
Leni Mayo <leni@learnable.com>
2013-06-30 06:37:11 +1000

    spec Configurable is in alphabetical order

--- spec/dummy/config/initializers/visit.rb
+++ spec/dummy/config/initializers/visit.rb
@@ -1,9 +1,8 @@
 module Visit
   Visit::Configurable.configure do |c|
-    c.labels_match_first = [
-      [  :get, %r{^/articles(?:\?.*|)$},   :articles_index ],
-      [  :get, %r{^/articles/(\d+)/(\d+)}, :subarticle     ],
-      [  :get, %r{^/articles/(\d+)},       :article        ],
+    c.cookies_match = [
+      /^a$/,
+      /^flip_/
     ]
 
     c.ignorable = [
@@ -11,14 +10,15 @@ module Visit
       /\/system\/blah/,
     ]
 
-    c.cookies_match = [
-      /^a$/,
-      /^flip_/
+    c.labels_match_all = c.labels_match_all.push *[
+      [ :get, %r{[?&]invite=(\w+)},   :invite         ],
+      [ :get, %r{[?&]trait_no_value}, :trait_no_value ],
     ]
 
-    c.labels_match_all = c.labels_match_all.push *[
-      [ :get, %r{[?&]invite=(\w+)},    :invite        ],
-      [ :get, %r{[?&]trait_no_value}, :trait_no_value ],
+    c.labels_match_first = [
+      [  :get, %r{^/articles(?:\?.*|)$},   :articles_index ],
+      [  :get, %r{^/articles/(\d+)/(\d+)}, :subarticle     ],
+      [  :get, %r{^/articles/(\d+)},       :article        ],
     ]
   end
 end
